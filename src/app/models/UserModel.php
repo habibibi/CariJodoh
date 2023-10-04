4f3fb916e0f393a258a69492260a66950c926862
Muhammad Habibi Husni <habibihusni18@gmail.com>
2023-10-04 22:23:22 +0700

    feat: add profiles fetch query and backend

--- src/app/models/UserModel.php
+++ src/app/models/UserModel.php
@@ -114,4 +114,130 @@ class UserModel
         // Return the user_id of the registered user
         return $userId;
     }
+
+    public function getProfiles($page = 1, $exclude_userid=null, $name=null, $interest=null, $agama=null, $mbti=null, $sortAttr='nama_lengkap', $isDesc=false)
+    {
+        $query = "
+        SELECT gambar_profile, nama_lengkap, domisili, hobi, interest, umur, tinggi_badan, agama, mbti 
+        FROM profile
+        ";
+        $cond = array();
+        if ($exclude_userid) {
+            array_push($cond," user_id != :userid");
+        }
+        if ($name) {
+            array_push($cond," nama_lengkap LIKE :name");
+        }
+        if ($interest) {
+            array_push($cond," interest LIKE :interest");
+        }
+        if ($agama) {
+            array_push($cond," agama = :agama");
+        }
+        if ($mbti) {
+            array_push($cond," mbti = :mbti");
+        }
+
+        if ($exclude_userid) {
+            $queryGender = "SELECT gender from profile WHERE user_id = :userid";
+            $this->database->query($queryGender);
+            $this->database->bind('userid', $exclude_userid);
+            $gender = $this->database->fetch()->gender;
+            array_push($cond, " gender != '". $gender . "'");
+        }
+
+        if (count($cond) > 0) {
+            $query .= " WHERE";
+            $query .= implode(" AND",$cond);
+        }
+
+        $query .= " ORDER BY " . $sortAttr;
+
+        if ($isDesc) {
+            $query .= " DESC";
+        } else {
+            $query .= " ASC";
+        }
+        $query .= " LIMIT 6 OFFSET :offset";
+        
+        $this->database->query($query);
+
+        if ($exclude_userid) {
+            $this->database->bind('userid', $exclude_userid);
+        }
+        if ($name) {
+            $this->database->bind('name', '%' . $name . '%');
+        }
+        if ($interest) {
+            $this->database->bind('interest', '%' . $interest . '%');
+        }
+        if ($agama) {
+            $this->database->bind('agama', $agama);
+        }
+        if ($mbti) {
+            $this->database->bind('mbti', $mbti);
+        }
+        
+        $this->database->bind('offset',($page-1)*6);
+        $profiles = $this->database->fetchall();
+        return $profiles;
+    }
+
+    public function getProfilesPageCount($exclude_userid=null, $name=null, $interest=null, $agama=null, $mbti=null)
+    {
+        $query = "
+        SELECT gambar_profile, nama_lengkap, domisili, hobi, interest, umur, tinggi_badan, agama, mbti 
+        FROM profile
+        ";
+        $cond = array();
+        if ($exclude_userid) {
+            array_push($cond," user_id != :userid");
+        }
+        if ($name) {
+            array_push($cond," nama_lengkap LIKE :name");
+        }
+        if ($interest) {
+            array_push($cond," interest LIKE :interest");
+        }
+        if ($agama) {
+            array_push($cond," agama = :agama");
+        }
+        if ($mbti) {
+            array_push($cond," mbti = :mbti");
+        }
+
+        if ($exclude_userid) {
+            $queryGender = "SELECT gender from profile WHERE user_id = :userid";
+            $this->database->query($queryGender);
+            $this->database->bind('userid', $exclude_userid);
+            $gender = $this->database->fetch()->gender;
+            array_push($cond, " gender != '". $gender . "'");
+        }
+
+        if (count($cond) > 0) {
+            $query .= " WHERE";
+            $query .= implode(" AND",$cond);
+        }
+        
+        $this->database->query($query);
+
+        if ($exclude_userid) {
+            $this->database->bind('userid', $exclude_userid);
+        }
+        if ($name) {
+            $this->database->bind('name', '%' . $name . '%');
+        }
+        if ($interest) {
+            $this->database->bind('interest', '%' . $interest . '%');
+        }
+        if ($agama) {
+            $this->database->bind('agama', $agama);
+        }
+        if ($mbti) {
+            $this->database->bind('mbti', $mbti);
+        }
+
+        $this->database->execute();
+        return ceil($this->database->rowCount()/6);
+    }
 }
