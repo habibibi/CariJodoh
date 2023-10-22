9c1673beda13fc52a80816f97f242bfd7a3daa3d
maikeljh <mjh191003@gmail.com>
2023-10-22 13:27:50 +0700

    feat: create middleware for routing + navbar footer + responsive home

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,15 @@
 import PropTypes from "prop-types";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const MainLayout = ({ children }) => {
   return (
-    <div className="bg-[#FFF8DC]">
-      <div className="max-w-screen-2xl mx-auto">{children}</div>
+    <div className="flex flex-col">
+      <Navbar />
+      <div className="bg-[#FFF8DC]">
+        <div className="max-w-screen-2xl mx-auto py-8 px-4">{children}</div>
+      </div>
+      <Footer />
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import Lock from "../assets/lock.webp";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -8,11 +9,16 @@ const Home = () => {
   };
 
   return (
-    <div className="text-5xl flex flex-col h-[100vh]">
-      <div className="mx-auto my-auto flex flex-col gap-8">
-        <h1>Welcome to CariJodoh Security Application</h1>
+    <div className="text-5xl flex flex-col min-h-[72vh] font-semibold">
+      <div className="mx-auto my-auto flex flex-col gap-12">
+        <h1 className="text-4xl text-center">
+          Welcome to CariJodoh Security Application
+        </h1>
+        <div className="mx-auto w-3/4 sm:w-1/3">
+          <img src={Lock} alt="" />
+        </div>
         <button
-          className="bg-[#FFD2DA] p-5 rounded-xl"
+          className="bg-[#FFD2DA] p-5 rounded-xl text-3xl"
           onClick={() => logout()}
         >
           Logout
